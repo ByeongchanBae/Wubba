7eebd9c927f916325b450c9f8c9cda20e4434ac0
Angus Williamson <angus.williamson16@gmail.com>
2021-03-09 16:44:12 +1030

    Finished user possibles method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users do
     resources :user_selections, only: [:new, :create, :edit, :update] do
       resources :tech_stack, only: [:show]
   end
-end
   resources :user_selections, only: [:destroy]
 end
