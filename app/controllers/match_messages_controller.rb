d3486aa7d55a94968f6b621ef12278c635e1e9da
Angus Williamson <angus.williamson16@gmail.com>
2021-03-12 14:07:44 +1030

    chat

--- app/controllers/match_messages_controller.rb
+++ app/controllers/match_messages_controller.rb
@@ -1,11 +1,11 @@
 class MatchMessagesController < ApplicationController
   def create
-    @matches = Match.find(params[:match_id])
+    @match = Match.find(params[:match_id])
     @match_message = MatchMessage.new(message_params)
-    @match_message.match = @matches
+    @match_message.match = @match
     @match_message.user = current_user
     if @match_message.save
-      redirect_to match_path(@match, anchor: "message-#{@message.id}")
+      redirect_to match_path(@match)
     else
       render "match/show"
     end
