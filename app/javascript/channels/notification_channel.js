71730777d59158440f1fa5211528bc5761b2526b
byeongchan baee <bbae0001@student.monash.edu>
2021-03-16 16:42:28 +1100

    implemented live notification sysystem

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -2,19 +2,24 @@ import consumer from "./consumer";
 
 const initNotificationCable = () => {
   const notificationsContainer = document.getElementById('notifications');
-  if (notificationsContainer) {
+  const notificationCountEl = document.querySelector('.js-notification-count');
+
     consumer.subscriptions.create({ channel: "NotificationChannel", user_id: currentUserId }, {
       received(data) {
-        notificationsContainer.insertAdjacentHTML('afterBegin', data)
+        if (notificationsContainer) {
+          notificationsContainer.insertAdjacentHTML('afterBegin', data);
+        }
+        if (notificationCountEl) {
+          const notificationCount = parseInt(notificationCountEl.innerText, 10);
+          notificationCountEl.innerText = notificationCount + 1;
+        }
         // PUT NOTIFICATION COUNT
         //  ADD EVENT LISTENER
         // update the bell button to 2 document.queyselector bell add classlist
-
       },
       connected() {
       }
     });
-  }
 }
 
 export { initNotificationCable };
